e959f305be06b45606e6c2165e0f67cb0e80d75a
Gyro Dev <gyrodefi@gmail.com>
2021-11-26 12:28:32 +0800

    chg: update copy

--- src/i18n.js
+++ src/i18n.js
@@ -26,7 +26,7 @@ i18n
           },
           hero: {
             heading: 'Build Wealth',
-            description: 'Weak hands peg, diamond hands HODL, smart hands hedge. What hands do you have?',
+            description: 'The safety of stable coins with the growth of traditional coins.',
             'start-app': 'Launch App',
             'buy-gyro': 'Buy GYRO'
           }
